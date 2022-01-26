eda3c50bb77096c9aed0b6282305827259c580bd
Ronny Trommer <ronny@opennms.org>
2022-01-26 11:40:21 +0100

    Add config map for scripts and rename namespace to opennms-poc

--- k8s-minimal/grafana/init-secrets.sh
+++ k8s-minimal/grafana/init-secrets.sh
@@ -2,4 +2,4 @@
 
 kubectl create secret generic grafana-secrets \
   --from-literal="GF_SECURITY_ADMIN_PASSWORD=kzqMpHG4vnX8QU8Z" \
-  -n chaos-engine
+  -n opennms-poc
--- k8s-minimal/onms-horizon/init-secrets.sh
+++ k8s-minimal/onms-horizon/init-secrets.sh
@@ -3,4 +3,4 @@
 kubectl create secret generic hzn-core-secrets \
   --from-literal="POSTGRES_PASSWORD=xQadUEDMc4ejp7p7" \
   --from-literal="OPENNMS_DBPASS=cglk1cMAJng6QBJ3Qhck" \
-  -n chaos-engine
+  -n opennms-poc
